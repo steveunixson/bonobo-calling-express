75228b62dc94247b1b6325f014c2a5486819db62
Steve Unixson <unixson@gmail.com>
2018-05-07 07:32:55 +0300

    fuck passport

--- src/config/express.js
+++ src/config/express.js
@@ -6,6 +6,7 @@ import config from '../config/mongodb'
 import mongoose from 'mongoose'
 import error from '../libs/error'
 import fileUpload from 'express-fileupload'
+import passport from 'passport'
 
 
 function App(app) {
@@ -17,6 +18,7 @@ function App(app) {
       //app.use(helmet());
       //app.disable('x-powered-by');
       app.use(morgan('dev'));
+      app.use(passport.initialize());
       mongoose.connect(config.database); // connect to database
         app.set('superSecret', config.secret); // secret variable
         //app.use(error)
--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ import express from 'express'
 import App from './config/express'
 import serve from 'express-static'
 import config from './config/mongodb'
+import login, { passportAll } from './controllers/passport'
+import passport from 'passport'
+
 
 const log = require('./libs/log')(module);
 const port = process.env.PORT || 8001;
@@ -12,5 +15,22 @@ const port = process.env.PORT || 8001;
 // Create an express server and a GraphQL endpoint
 var app = express();
 new App(app)
+
+app.post('/api/login', login.getUsers)
+
+app.post('/api/signin', login.postUsers)
+
+app.all('*', passportAll)
+
+//all routes from this point are now protected by passport
+
+app.get('/message', passport.authenticate('bearer', { session: false }), function(req, res) {
+     res.send('ok')
+  });
+  
+app.use(function(err, req, res, next) {
+    console.error(err);
+    return res.status(403).json({ status: 'error', code: 'unauthorized' });
+  });
 
 app.listen(port, () => log.info('Calling Bonobo Now Running On :' + port));
