517da1ec366270fde46df0ddda3e8599b4a83ea3
Steve Unixson <unixson@gmail.com>
2018-05-07 03:55:44 +0300

    added main structure

---
+++ src/config/express.js
@@ -0,0 +1,27 @@
+import bodyParser from 'body-parser'
+import compression from 'compression'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import config from '../config/mongodb'
+import mongoose from 'mongoose'
+import error from '../libs/error'
+import fileUpload from 'express-fileupload'
+
+
+function App(app) {
+    app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(bodyParser.json()) // parse application/json
+    app.use(compression({
+        threshold: 512
+      }));
+      //app.use(helmet());
+      //app.disable('x-powered-by');
+      app.use(morgan('dev'));
+      mongoose.connect(config.database); // connect to database
+        app.set('superSecret', config.secret); // secret variable
+        //app.use(error)
+        app.use(fileUpload());
+
+}
+
+module.exports = App;
